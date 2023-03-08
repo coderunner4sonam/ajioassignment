8a6844d51a62f0890ec32c3497831e683ee1c3e5
Sonam <sonammurarkar41@gmail.com>
2023-03-08 12:46:30 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import ImageSlider from './ImageSlider';
-import Slide from './Slide';
+
 
 function App() {
   return (
     <div>
       <Header/>
       <ImageSlider/>
-      <Slide/>
+     
     </div>
   );
 }
